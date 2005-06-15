aa4bb54df302b0fa46b7460fe725eecfaae2c9b0
axxman <>
2005-06-15 21:48:01 +0000

    Now getting third party components from bombardierinstaller.org

--- client/preCompile.sh
+++ client/preCompile.sh
@@ -6,20 +6,23 @@ cd release/bombardier-0.4
 echo "PyCurl"
 mkdir pycurl-ssl-7.13.2.win32-py2.4
 cd pycurl-ssl-7.13.2.win32-py2.4
+curl http://www.bombardierinstaller.org/downloads/pycurl-ssl-7.13.2.win32-py2.4.exe > ../../pycurl-ssl-7.13.2.win32-py2.4.exe
 unzip ../../pycurl-ssl-7.13.2.win32-py2.4.exe > unzip.txt
 cd ..
 
 echo "pyyaml-46"
+curl http://www.bombardierinstaller.org/downloads/pyyaml-46.tar.gz > ../pyyaml-46.tar.gz
 tar -xzf ../pyyaml-46.tar.gz
 
 echo "Windows extensions"
 mkdir pywin32-204.win32-py2.4
 cd pywin32-204.win32-py2.4
+curl http://www.bombardierinstaller.org/downloads/pywin32-204.win32-py2.4.exe > ../../pywin32-204.win32-py2.4.exe
 unzip ../../pywin32-204.win32-py2.4.exe > unzip.txt
 cd ..
 
 echo "wx windows"
-cp ../wxPython2.6-win32-unicode-2.6.0.0-py24.exe .
+curl http://www.bombardierinstaller.org/downloads/wxPython2.6-win32-unicode-2.6.0.0-py24.exe > wxPython2.6-win32-unicode-2.6.0.0-py24.exe
 
 echo "bombardierClient"
 mkdir bombardierClient
