f17d4ff2a6f4765c6b1999fb64250454f1842433
peter <>
2005-06-15 23:38:02 +0000

    made postcompile more externally friendly

--- client/postCompile.sh
+++ client/postCompile.sh
@@ -2,5 +2,5 @@
 set +x
 
 cp Output/bombardierSetup-0.4.exe release/bombardierSetup-0.4.exe
-scp release/bombardier-0.4.tar.gz release/bombardierSetup-0.4.exe root@172.17.50.56:/var/www/deploy
+scp release/bombardier-0.4.tar.gz release/bombardierSetup-0.4.exe `cat release/server.txt`
 
