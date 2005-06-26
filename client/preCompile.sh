8ab23645c78b1fb2956b5d0e8baff1624dbf7763
peter <>
2005-06-26 07:11:26 +0000

    Accidentally uncommented curl commands

--- client/preCompile.sh
+++ client/preCompile.sh
@@ -12,28 +12,28 @@ mkdir release/bombardier-0.4
 cd release/bombardier-0.4
 
 echo "python-2.4.1" 
-#curl $downloads/python-2.4.1.msi > ../python-2.4.1.msi
+curl $downloads/python-2.4.1.msi > ../python-2.4.1.msi
 
 echo "PyCurl"
 mkdir pycurl-ssl-7.13.2.win32-py2.4
 cd pycurl-ssl-7.13.2.win32-py2.4
-##curl $downloads/pycurl-ssl-7.13.2.win32-py2.4.exe > ../../pycurl-ssl-7.13.2.win32-py2.4.exe
+curl $downloads/pycurl-ssl-7.13.2.win32-py2.4.exe > ../../pycurl-ssl-7.13.2.win32-py2.4.exe
 unzip ../../pycurl-ssl-7.13.2.win32-py2.4.exe > unzip.txt
 cd ..
 
 echo "pyyaml-46"
-##curl $downloads/pyyaml-46.tar.gz > ../pyyaml-46.tar.gz
+url $downloads/pyyaml-46.tar.gz > ../pyyaml-46.tar.gz
 tar -xzf ../pyyaml-46.tar.gz
 
 echo "Windows extensions"
 mkdir pywin32-204.win32-py2.4
 cd pywin32-204.win32-py2.4
-#curl $downloads/pywin32-204.win32-py2.4.exe > ../../pywin32-204.win32-py2.4.exe
+curl $downloads/pywin32-204.win32-py2.4.exe > ../../pywin32-204.win32-py2.4.exe
 unzip ../../pywin32-204.win32-py2.4.exe > unzip.txt
 cd ..
 
 echo "wx windows"
-#curl $downloads/wxPython2.6-win32-unicode-2.6.0.0-py24.exe > wxPython2.6-win32-unicode-2.6.0.0-py24.exe
+curl $downloads/wxPython2.6-win32-unicode-2.6.0.0-py24.exe > wxPython2.6-win32-unicode-2.6.0.0-py24.exe
 
 echo "bombardierClient"
 mkdir bombardierClient
