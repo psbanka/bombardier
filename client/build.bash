c8a220ae9ce20525f141c1dc95af40fdf8d9f805
axxman <>
2005-06-29 21:55:17 +0000

    Changed error codes

--- client/build.bash
+++ client/build.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd ../TEST || exit 1
-/cygdrive/c/Python24/python.exe unitTests.py || exit 2
+/cygdrive/c/Python24/python.exe unitTests.py || exit 1
 
-cd ../client || exit 3
+cd ../client || exit 1
 
-bash ./preCompile.sh || exit 4
+bash ./preCompile.sh || exit 1
 
-/cygdrive/c/Program\ Files/Inno\ Setup\ 5/Compil32.exe /cc inno-setup2.iss || exit 5
+/cygdrive/c/Program\ Files/Inno\ Setup\ 5/Compil32.exe /cc inno-setup2.iss || exit 1
 
 bash ./postCompile.sh
+
