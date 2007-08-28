2dd3db12941e6168be7059ae65736aa5df7c612f
shawn.sherwood <>
2007-08-28 16:19:22 +0000

    Added glob for name of tarball.

--- client/postCompile.sh
+++ client/postCompile.sh
@@ -2,5 +2,5 @@
 set +x
 
 cp Output/bombardierSetup-$BOMBARDIER_VERSION*.exe release
-scp release/bombardier-$BOMBARDIER_VERSION.tar.gz release/bombardierSetup-$BOMBARDIER_VERSION*.exe `cat release/server.txt`
+scp release/bombardier-$BOMBARDIER_VERSION*.tar.gz release/bombardierSetup-$BOMBARDIER_VERSION*.exe `cat release/server.txt`
 
