457ef4070502ea7b146e6c1d9f924e94114a87c5
peter.banka <>
2007-11-02 21:22:21 +0000

    added functions for HostAuthorization

--- client/bombardier/Linux.py
+++ client/bombardier/Linux.py
@@ -22,6 +22,91 @@
 import OperatingSystem, os
 import Logger
 from staticData import *
+import re
+from commands import getstatusoutput
+
+def md5crypt(password, salt='', magic='$1$'):
+    import md5, random
+    # Based on FreeBSD src/lib/libcrypt/crypt.c 1.2
+    # http://www.freebsd.org/cgi/cvsweb.cgi/~checkout~/src/lib/libcrypt/crypt.c?rev=1.2&content-type=text/plain
+
+    # Original license:
+    # * "THE BEER-WARE LICENSE" (Revision 42):
+    # * <phk@login.dknet.dk> wrote this file.  As long as you retain this notice you
+    # * can do whatever you want with this stuff. If we meet some day, and you think
+    # * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+
+    # This port adds no further stipulations.  I forfeit any copyright interest.
+    itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if not salt:
+        salt = random.sample(itoa64, 8)
+    m = md5.new()
+    m.update(password + magic + salt)
+    mixin = md5.md5(password + salt + password).digest()
+    for i in range(0, len(password)):
+        m.update(mixin[i % 16])
+    i = len(password)
+    while i:
+        if i & 1:
+            m.update('\x00')
+        else:
+            m.update(password[0])
+        i >>= 1
+    final = m.digest()
+    for i in range(1000):
+        m2 = md5.md5()
+        if i & 1:
+            m2.update(password)
+        else:
+            m2.update(final)
+        if i % 3:
+            m2.update(salt)
+        if i % 7:
+            m2.update(password)
+        if i & 1:
+            m2.update(final)
+        else:
+            m2.update(password)
+        final = m2.digest()
+    rearranged = ''
+    for a, b, c in ((0, 6, 12), (1, 7, 13), (2, 8, 14), (3, 9, 15), (4, 10, 5)):
+        v = ord(final[a]) << 16 | ord(final[b]) << 8 | ord(final[c])
+        for i in range(4):
+            rearranged += itoa64[v & 0x3f]; v >>= 6
+    v = ord(final[11])
+    for i in range(2):
+        rearranged += itoa64[v & 0x3f]; v >>= 6
+    return magic + salt + '$' + rearranged
+
+def getUserAliases(data):
+    c = re.compile("User_Alias (\S+) += *(.+)")
+    output = {"ADMINS":set(), "DEVS":set()}
+    groups = c.findall(data)
+    for group in groups:
+        groupName = group[0]
+        output[groupName] = set([ x.strip() for x in group[1].split(',') ])
+    return output
+
+def setUserAliases(data, aliases):
+    c = re.compile("User_Alias (\S+) += *(.+)")
+    output = []
+    for groupName in aliases:
+        output.append("User_Alias %s = %s" % (groupName, ', '.join(aliases[groupName])))
+    for line in data.split('\n'):
+        groups = c.findall(line)
+        if not groups:
+            output.append(line)
+    return output
+
+def modifySudoers(username, type, filename="/etc/sudoers"):
+    data = open(filename).read()
+    aliases = getUserAliases(data)
+    if type == ADMIN_USER:
+        aliases["ADMINS"].add(username)
+    if type == DEV_USER:
+        aliases["DEVS"].add(username)
+    open(filename, 'w').write('\n'.join(setUserAliases(data, aliases)))
+
 
 class Linux(OperatingSystem.OperatingSystem):
 
@@ -44,6 +129,31 @@ class Linux(OperatingSystem.OperatingSystem):
                               workingDirectory = workingDirectory, captureOutput = True)
         return status
     
+    def listAllUsers(self):
+        status, output = getstatusoutput( "getent passwd | awk -F : '{print$1}'" )
+        if status != OK:
+            raise Exception( "Could not get user list." )
+        return output.split( '\n' )
+        
+    def removeUser(self, username):
+        status,output = getstatusoutput('userdel %s 2>&1' %username )
+        if status != OK:
+            raise Exception( "Error deleting %s: %s" %(username, output) )
+        return status
+
+    def createUser(self, username, password, type, comment=''):
+        md5Pass = md5crypt(password)
+        if type not in [SSH_USER, ADMIN_USER, DEV_USER]:
+            shell = "/bin/false"
+        else:
+            shell = "/bin/bash"
+        cmd = "useradd -p %s -s %s %s" % (md5Pass, shell, username)
+        status,output = getstatusoutput(cmd)
+        if status != OK:
+            raise Exception( "Error creating %s: %s" %(username, output) )
+        modifySudoers(username, type)
+        return status
+   
     def noRestartOnLogon(self): 
         pass
 
