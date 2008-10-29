9cfe394c6523cd0ccef28ed8a5a477acb4e591d6
root <root@liberator>
2008-10-29 02:26:47 -0700

    Possibly corrected error in make.sh

--- client/make.sh
+++ client/make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $(bzr stat | awk '/^modified:/' | wc -l) ]; then
+if [ $(bzr stat | awk '/^modified:/' | wc -l) != 0 ]; then
     echo "Check in and commit changes to upstream branch before building."
     exit 1
 fi
-if [ $(bzr info | awk '/^Checkout .*/' | wc -l) ]; then
+if [ $(bzr info | awk '/^Checkout .*/' | wc -l) != 0 ]; then
     bzr version-info --format python > lib/_version.py
     python setup.py sdist
 else:
