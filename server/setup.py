5684cd7eecd243d8c50e72ae4c20dd2cecbb710e
root <root@peacemaker>
2009-12-17 16:06:37 -0800

    Got templates to install properly

--- server/setup.py
+++ server/setup.py
@@ -2,6 +2,7 @@
 
 from distutils.core import setup
 from lib._version import version_info
+import glob
 setup (name = "bombardier_server",
        description = "Open Source Configuration management "\
                      "and package delivery: central server components",
@@ -12,6 +13,8 @@ setup (name = "bombardier_server",
        package_data = {"bombardier_server": ["web/cnm/templates/*.html",
                                              "web/cnm/templates/registration/*.html"]},
        package_dir = {"bombardier_server": "lib", "django_restapi": "lib/django_restapi"},
+       data_files = [("web/cnm/templates", glob.glob("web/cnm/templates/*.html")),
+                     ("web/cnm/templates/registration", glob.glob("web/cnm/templates/registration/*.html"))],
        scripts = ["scripts/bdr", "scripts/dsl", "scripts/ymlEncrypter.py"],
        author = "Peter Banka and Shawn Sherwood",
        author_email = "peter.banka@gmail.com",
