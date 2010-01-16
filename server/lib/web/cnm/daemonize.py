48aadc5d7a15167bb2aa5864233fc6afa1cdf0f5
root <root@peacemaker>
2010-01-15 16:29:08 -0800

    Added daemonize, which wasn't under revision control, and fixed up setup.py

---
+++ server/lib/web/cnm/daemonize.py
@@ -0,0 +1,68 @@
+"Script to run dispatcher as a daemon"
+import os, sys, time
+from django.core.management import setup_environ
+from Dispatcher import Dispatcher
+import settings, resource, yaml
+import Pyro.core
+from bombardier_core.static_data import OK
+MAXFD = 1024
+INFO_FILE = "dispatcher_info.yml"
+
+def refork():
+    "Inner fork, sets up Dispatcher"
+    os.setsid()
+    try:
+        pid = os.fork()
+    except OSError, err:
+        raise Exception, "%s [%d]" % (err.strerror, err.errno)
+
+    if (pid != 0):
+        while not os.path.isfile(INFO_FILE):
+            time.sleep(1)
+        os._exit(0)
+    else:
+        setup_environ(settings)
+        Pyro.core.initServer()
+        daemon = Pyro.core.Daemon()
+        uri = daemon.connect(Dispatcher(),"dispatcher")
+        pid = os.getpid()
+        info_dict = { "dispatcher_uri": str(uri), "dispatcher_pid": pid }
+        open(INFO_FILE, 'w').write( yaml.dump(info_dict) )
+        daemon.requestLoop()
+
+def daemonize():
+    "Create a deamon process from Dispatcher"
+    if os.path.isfile(INFO_FILE):
+        os.unlink(INFO_FILE)
+    try:
+        pid = os.fork()
+    except OSError, err:
+        raise Exception, "%s [%d]" % (err.strerror, err.errno)
+
+    if (pid == 0):
+        refork()
+    else:
+        while not os.path.isfile(INFO_FILE):
+            time.sleep(1)
+        return OK
+
+    while not os.path.isfile(INFO_FILE):
+        time.sleep(1)
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if (maxfd == resource.RLIM_INFINITY):
+        maxfd = MAXFD
+  
+    for file_descriptor in range(0, maxfd):
+        try:
+            os.close(file_descriptor)
+        except OSError:
+            pass
+
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+
+    return OK
+
+if __name__ == "__main__":
+    sys.exit(daemonize())
+
--- server/setup.py
+++ server/setup.py
@@ -1,4 +1,67 @@
-# setup.py: sets bombardier up.
+#!/usr/bin/env python
+
+"sets bombardier_server up."
+
+from distutils.core import Command, setup
+from unittest import TextTestRunner, TestLoader
+from glob import glob
+from os.path import splitext, basename, join as pjoin, walk
+import os
+from lib._version import version_info
+
+LONG_DESCRIPTION = """\
+Bombardier is a software system that delivers visibility, control
+and automation to datacenter environments. Bombardier provides a means
+for changes to be rolled out to a network of servers in a highly
+controlled way, providing optimum security, logging, centralized
+change control.
+
+"""
+
+
+class CleanCommand(Command):
+    user_options = [ ]
+    def initialize_options(self):
+        self._clean_me = [ ]
+        for root, dirs, files in os.walk('.'):
+            for f in files:
+                if f.endswith('.pyc'):
+                    self._clean_me.append(pjoin(root, f))
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for clean_me in self._clean_me:
+            try:
+                os.unlink(clean_me)
+            except:
+                pass
+
+class TestCommand(Command):
+    user_options = [ ]
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+        testfiles = [ ]
+        for t in glob(pjoin(self._dir, 'tests', '*.py')):
+            if not t.endswith('__init__.py'):
+                testfiles.append('.'.join(
+                    ['tests', splitext(basename(t))[0]])
+                )
+
+        tests = TestLoader().loadTestsFromNames(testfiles)
+        t = TextTestRunner(verbosity = 1)
+        t.run(tests)
+
+
 
 from distutils.core import setup
 from lib._version import version_info
@@ -17,6 +80,9 @@ setup (name = "bombardier_server",
                      ("web/cnm/templates/registration", glob.glob("web/cnm/templates/registration/*.html"))],
        author = "Peter Banka and Shawn Sherwood",
        author_email = "peter.banka@gmail.com",
+       long_description = LONG_DESCRIPTION,
+       install_requires = [ 'PyYAML', 'bombardier_core', "Pyro", "Django", "pexpect" ],
+       provides = [ 'bombardier_server' ],
        url = "http://bombardierinstaller.org/",
        classifiers = [
           "Development Status :: 2 - Pre-Alpha",
