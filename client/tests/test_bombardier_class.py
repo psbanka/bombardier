4dc382d736a4a9488c6bd95936f8537e5c1bd0fd
shawn.sherwood <shawn.sherwood@peacemaker>
2010-08-03 11:21:23 -0700

    Using a created temp dir to avoid permissions problems with testing.

--- client/tests/test_bombardier_class.py
+++ client/tests/test_bombardier_class.py
@@ -397,6 +397,7 @@ class BombardierTest(unittest.TestCase):
         3. change the configs and see if the system notices
         """
         # STEP 1
+        dest_dir = tempfile.mkdtemp()
         pkg_data = {"TestPackage": {"install": {"fullName":"TestPackage-7"},
                                     "package-version": 4,
                                     "configuration": { "test": {"value": "abc123",
@@ -413,7 +414,7 @@ class BombardierTest(unittest.TestCase):
 
         # STEP 2
         self.config.data = {"packages": ["TestPackage"],
-                            "test": {"value": 'abc', "directory": "/tmp"},
+                            "test": {"value": 'abc', "directory": dest_dir},
                            }
         self.bombardier.config = self.config
         status = self.bombardier.reconcile_system(RECONCILE)
@@ -421,10 +422,11 @@ class BombardierTest(unittest.TestCase):
 
         # STEP 3
         self.config.data = {"packages": ["TestPackage"],
-                            "test": {"value": 'DEF', "directory": "/tmp"},
+                            "test": {"value": 'DEF', "directory": dest_dir},
                            }
         self.bombardier.config = self.config
         test_results = self.bombardier.check_system()
+        os.system("rm -rf %s" % dest_dir)
         assert set(test_results['Packages installed properly']) == set()
         assert set(test_results['Packages to be INSTALLED']) == set(), test_results
         assert set(test_results['Packages to be REMOVED']) == set()
